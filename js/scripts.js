3a72ad703aa3583a3585833753ed94a875457b89
beryl2 <berylakinyi150@gmail.com>
2022-01-13 14:25:58 +0300

    initialse commit

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+    $("div#click-one").click(function(event){
+        var whatToSay = "hello!";
+        alert(whatToSay);
+    });
+
+    $("div#click-two").click(function(event) {
+        var whatToSay ="hello!";
+        alert(whatToSay);
+    });
+
+    $("div#click-two").click(function(event) {
+        alert(whatToSay);
+    });
+});
